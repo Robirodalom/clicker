c139265d51a0ca4ec9ad97016ea1b9c447ff867f
Wagner Robert <wagrob96@gmail.com>
2018-10-31 00:33:57 +0100

    added method for clicking

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 new Vue({
 el: '#vue-app',
 data: {
-  actualCoockies: 0,
-  sumCoockies: 0,
-  clickCoockie: 0,
-  buildingCoockie: 0
+  actualCookies: 0,
+  sumCookies: 0,
+  clickCookie: 1,
+  buildingCookie: 0
 },
 methods: {
-
+  clickMainCookie: function(){
+    this.actualCookies += this.clickCookie;
+    this.sumCookies += this.clickCookie;
+  }
 }
 
 
