2a7edc965d1ec8cf45a537aa1b9da7a866ad1485
Wagner Robert <wagrob96@gmail.com>
2018-11-08 22:17:49 +0100

    All building buttons are working

--- src/components/app.js
+++ src/components/app.js
@@ -7,10 +7,10 @@ data: {
   buildingCookie: 0,
   bonusPercent: 1.0,
   buildingsInfo: [
-    {name: 'Henagon', ammount: 0, cost: 10, baseEarn: 1, upgrades: 0}, {name: 'Diagon', ammount: 0, cost: 105, baseEarn: 11, upgrades: 0},
-    {name: 'Triangle', ammount: 0, cost: 1100, baseEarn: 1150, upgrades: 0}, {name: 'Quadrilateral', ammount: 0, cost: 11500, baseEarn: 12100, upgrades: 0},
-    {name: 'Pentagon', ammount: 0, cost: 121000, baseEarn: 126500, upgrades: 0}, {name: 'Hexagon', ammount: 0,cost: 1270000, baseEarn: 1331000, upgrades: 0},
-    {name: 'Heptagon', ammount: 0, cost: 13335000, baseEarn: 13970000, upgrades: 0}, {name: 'Octagon', ammount: 0, cost: 140000000, baseEarn: 146685000, upgrades: 0}
+    {name: 'Henagon', ammount: 0, cost: 10, baseEarn: 0.1, upgrades: 0}, {name: 'Diagon', ammount: 0, cost: 105, baseEarn: 1, upgrades: 0},
+    {name: 'Triangle', ammount: 0, cost: 1100, baseEarn: 11, upgrades: 0}, {name: 'Quadrilateral', ammount: 0, cost: 11500, baseEarn: 120, upgrades: 0},
+    {name: 'Pentagon', ammount: 0, cost: 121000, baseEarn: 1300, upgrades: 0}, {name: 'Hexagon', ammount: 0,cost: 1270000, baseEarn: 12500, upgrades: 0},
+    {name: 'Heptagon', ammount: 0, cost: 13335000, baseEarn: 140000, upgrades: 0}, {name: 'Octagon', ammount: 0, cost: 140000000, baseEarn: 14000000, upgrades: 0}
   ],
   statisticInfo: [
     'Cookies backed all-time:', 'Cookies clicked all-time:', 'Handmade cookies:', 'Buildings owned:'
@@ -27,7 +27,7 @@ mounted: function () {
         this.$nextTick(function() {
           window.setInterval(() => {
             this.addEarnings();
-          },250);
+          },1000);
         })
   },
 
@@ -37,32 +37,35 @@ methods: {
     this.sumCookies += this.clickCookie;
   },
   buyBuilding: function(tmpName){
-    if(tmpName == "Henagon"){
-      if(this.buildingsInfo[0].cost <= this.actualCookies){
-        this.actualCookies -= this.buildingsInfo[0].cost;
-        this.buildingsInfo[0].ammount++;
-        this.buildingsInfo[0].cost*= 1.15;
-        this.buildingsInfo[0].cost = parseFloat(this.buildingsInfo[0].cost.toFixed(0));
+    var i;
+    for(i = 0; i < 8; i++){
+
+    if(tmpName == this.buildingsInfo[i].name){
+      if(this.buildingsInfo[i].cost <= this.actualCookies){
+        this.actualCookies -= this.buildingsInfo[i].cost;
+        this.buildingsInfo[i].ammount++;
+        this.buildingsInfo[i].cost*= 1.15;
+        this.buildingsInfo[i].cost = parseFloat(this.buildingsInfo[i].cost.toFixed(0));
       }
     }
-    else if(tmpName == "Diagon"){
-      this.buildingsInfo[1].ammount++;
-    }
+  }
+
   },
+
   updateEarning: function(){
     this.buildingCookie = 0;
     var i;
     for(i = 0; i < 8; i++){
-      this.buildingCookie += parseFloat( (this.buildingsInfo[i].baseEarn * this.buildingsInfo[i].ammount * (this.buildingsInfo[i].upgrades + 1) * this.bonusPercent).toFixed(0));
+      this.buildingCookie += parseFloat( (this.buildingsInfo[i].baseEarn * this.buildingsInfo[i].ammount * (this.buildingsInfo[i].upgrades + 1) * this.bonusPercent).toFixed(1));
     }
   },
+
   addEarnings: function(){
-    this.actualCookies += this.buildingCookie / 4;
-    this.sumCookies += this.buildingCookie / 4;
+    this.actualCookies += this.buildingCookie;
+    this.sumCookies += this.buildingCookie;
 
     this.actualCookies = parseFloat(this.actualCookies.toFixed(2));
     this.sumCookies = parseFloat(this.sumCookies.toFixed(2));
-
   }
 }
 
